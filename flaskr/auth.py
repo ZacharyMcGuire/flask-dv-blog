dd8bf3e967c8a2ec08d126f7b3003bff53333eb7
Zachary McGuire <zachary_mcguire@outlook.com>
2020-08-03 19:51:34 +1000

    Update auth.py
    Resolved bug where user session was not loaded.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,6 +10,29 @@ from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -25,12 +48,12 @@ def register():
         elif db.execute(
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
-            error = 'User {} is already registered.'.format(username)
+            error = 'User {0} is already registered.'.format(username)
 
         if error is None:
             db.execute(
                 'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
+                (username, generate_password_hash(password)),
             )
             db.commit()
             return redirect(url_for('auth.login'))
@@ -51,7 +74,9 @@ def login():
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
-        if user is None or not check_password_hash(user['password'], password):
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user['password'], password):
             error = 'Incorrect username or password.'
 
         if error is None:
@@ -68,13 +93,3 @@ def login():
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
-
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
-
-        return view(**kwargs)
-    return wrapped_view
