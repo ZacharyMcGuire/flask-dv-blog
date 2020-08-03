8a635ec4d9b07e9aded17efbc196b05a1050f221
Zachary McGuire <zachary_mcguire@outlook.com>
2020-08-03 20:37:44 +1000

    test coverage
    Added 100% test coverage for flaskr

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -77,7 +77,7 @@ def login():
         if user is None:
             error = "Incorrect username."
         elif not check_password_hash(user['password'], password):
-            error = 'Incorrect username or password.'
+            error = 'Incorrect password.'
 
         if error is None:
             session.clear()
