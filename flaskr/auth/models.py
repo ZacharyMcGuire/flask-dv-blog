33e08b8c4f719182f6f4e3d952af497cd2c43f6b
Zachary McGuire <zachary_mcguire@outlook.com>
2020-08-05 20:44:40 +1000

    created initial DV model

--- flaskr/auth/models.py
+++ flaskr/auth/models.py
@@ -1,11 +1,60 @@
+import datetime
+
 from sqlalchemy.ext.hybrid import hybrid_property
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr import db
+from flaskr import db, create_hash
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password = db.Column("password", db.String, nullable=False)
+
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        """Store the password as a hash for security."""
+        self._password = generate_password_hash(value)
+
+    def check_password(self, value):
+        return check_password_hash(self.password, value)
+
+
+class HubUser(db.Model):
+    __tablename__ = 'hub_user'
+    _user_hash_key = db.Column(
+        "user_hash_key", db.String(64), primary_key=True)
+    created = db.Column(
+        db.DateTime, nullable=False, index=True,
+        default=datetime.datetime.utcnow(),
+    )
+    user_id = db.Column(db.Integer, nullable=False, index=True, unique=True)
+
+    @hybrid_property
+    def user_hash_key(self):
+        """return the user hash key"""
+        return self._user_hash_key
+
+    @user_hash_key.setter
+    def user_hash_key(self, user_id):
+        """hash the `user_id` and save the result"""
+        new_hash_key = create_hash(user_id)
+        self._user_hash_key = new_hash_key
+
+
+class SatUserInfo(db.Model):
+    __tablename__ = 'sat_user_info'
+    user_hash_key = db.Column(
+        db.ForeignKey(HubUser.user_hash_key), nullable=False, primary_key=True
+    )
+    created = db.Column(
+        db.DateTime, nullable=False, primary_key=True,
+        default=datetime.datetime.utcnow(),
+    )
     username = db.Column(db.String, unique=True, nullable=False)
     _password = db.Column("password", db.String, nullable=False)
 
--- flaskr/blog/models.py
+++ flaskr/blog/models.py
@@ -1,7 +1,11 @@
+import datetime
+
+from sqlalchemy.ext.hybrid import hybrid_property
+
 from flask import url_for
 
-from flaskr import db
-from flaskr.auth.models import User
+from flaskr import db, create_hash
+from flaskr.auth.models import User, HubUser
 
 
 class Post(db.Model):
@@ -24,3 +28,70 @@ class Post(db.Model):
     @property
     def delete_url(self):
         return url_for("blog.delete", id=self.id)
+
+
+class HubPost(db.Model):
+    __tablename__ = 'hub_post'
+    _post_hash_key = db.Column(
+        "post_hash_key", db.String(64), primary_key=True)
+    created = db.Column(
+        db.DateTime, nullable=False, index=True,
+        default=datetime.datetime.utcnow(),
+    )
+    post_id = db.Column(db.Integer, nullable=False, index=True, unique=True)
+
+    author = db.relationship(
+        'hub_user', secondary='link_author', backref='hub_post'
+    )
+
+    @hybrid_property
+    def post_hash_key(self):
+        """return the post hash key"""
+        return self._post_hash_key
+
+    @post_hash_key.setter
+    def post_hash_key(self, post_id):
+        """hash the `post_id` and save the result"""
+        new_hash_key = create_hash(post_id)
+        self._post_hash_key = new_hash_key
+
+
+class LinkAuthor(db.Model):
+    __tablename__ = 'link_author'
+    _author_hash_key = db.Column(
+        "author_hash_key", db.String(64), primary_key=True
+    )
+    created = db.Column(
+        db.DateTime, nullable=False, index=True,
+        default=datetime.datetime.utcnow(),
+    )
+    post_hash_key = db.Column(
+        db.ForeignKey(HubPost.post_hash_key), nullable=False
+    )
+    user_hash_key = db.Column(
+        db.ForeignKey(HubUser.user_hash_key), nullable=False
+    )
+
+    @hybrid_property
+    def author_hash_key(self):
+        """Return the author hash key"""
+        return self._author_hash_key
+
+    @author_hash_key.setter
+    def author_hash_key(self, post_hash_key, user_hash_key):
+        """create a hash key and save it"""
+        new_hash_key = create_hash(post_hash_key, user_hash_key)
+        self._author_hash_key = new_hash_key
+
+
+class SatPostContent(db.Model):
+    __tablename__ = 'sat_post_content'
+    post_hash_key = db.Column(
+        db.ForeignKey(HubPost.post_hash_key), nullable=False, primary_key=True
+    )
+    created = db.Column(
+        db.DateTime, nullable=False, primary_key=True,
+        default=datetime.datetime.utcnow(),
+    )
+    title = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
